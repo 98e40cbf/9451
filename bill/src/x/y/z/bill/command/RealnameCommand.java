efa412906af975fe80020a605cc9dccf0415e531
9452 <zcteng@163.com>
2016-07-09 18:18:24 +0800

    006

---
+++ bill/src/x/y/z/bill/command/RealnameCommand.java
@@ -0,0 +1,31 @@
+package x.y.z.bill.command;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import io.alpha.core.model.BaseObject;
+
+public class RealnameCommand extends BaseObject {
+
+    private static final long serialVersionUID = 5539652850230952027L;
+    @NotEmpty
+    private String realName;
+    @NotEmpty
+    private String idCardNo;
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(final String realName) {
+        this.realName = realName;
+    }
+
+    public String getIdCardNo() {
+        return idCardNo;
+    }
+
+    public void setIdCardNo(final String idCardNo) {
+        this.idCardNo = idCardNo;
+    }
+
+}
