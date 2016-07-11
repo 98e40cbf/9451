2839e645be0dc4dfae7dd5d71a4706745175b364
9452 <zcteng@163.com>
2016-07-11 14:39:58 +0800

    #08

--- bill/src/x/y/z/bill/service/account/LoginHistoryService.java
+++ bill/src/x/y/z/bill/service/account/LoginHistoryService.java
@@ -1,18 +1,23 @@
 package x.y.z.bill.service.account;
 
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicReference;
 
 import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
 import io.alpha.akka.Actors;
+import io.alpha.core.config.ProcessorHelper;
 import io.alpha.core.util.ApplicationContextHolder;
 import x.y.z.bill.mapper.account.LoginHistoryDAO;
 import x.y.z.bill.model.account.LoginHistory;
 
 public class LoginHistoryService extends UntypedActor {
 
-    private static final ActorRef R = Actors.actorOf(0, Props.create(LoginHistoryService.class), "login:history-actor");
+    private static final ActorRef R = Actors.actorOf(ProcessorHelper.duple(), Props.create(LoginHistoryService.class),
+            "login:history-actor");
+
+    private static final AtomicReference<LoginHistoryDAO> DAO = new AtomicReference<>();
 
     public static void checkIn(final long userId, final long loginIp, final String browser) {
         LoginHistory loginHistory = new LoginHistory();
@@ -24,16 +29,25 @@ public class LoginHistoryService extends UntypedActor {
         R.tell(loginHistory, ActorRef.noSender());
     }
 
-    private void checkIn0(final LoginHistory loginHistory) {
-        ApplicationContextHolder.getBean(LoginHistoryDAO.class).insert(loginHistory);
-    }
-
     @Override
     public void onReceive(final Object arg0) throws Exception {
         if (arg0 instanceof LoginHistory) {
-            checkIn0((LoginHistory) arg0);
+            get().insert((LoginHistory) arg0);
         } else {
             unhandled(arg0);
+        }
+    }
+
+    private LoginHistoryDAO get() {
+        for (;;) {
+            LoginHistoryDAO dao = DAO.get();
+            if (dao != null) {
+                return dao;
+            }
+            dao = ApplicationContextHolder.getBean(LoginHistoryDAO.class);
+            if (DAO.compareAndSet(null, dao)) {
+                return dao;
+            }
         }
     }
 
