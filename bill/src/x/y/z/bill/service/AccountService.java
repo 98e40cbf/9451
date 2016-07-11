8eb921ab189aa6d0a7e73b402ba74f72a7f215fc
9452 <zcteng@163.com>
2016-07-11 11:03:15 +0800

    #01

--- bill/src/x/y/z/bill/service/AccountService.java
+++ bill/src/x/y/z/bill/service/AccountService.java
@@ -5,11 +5,14 @@ import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import io.alpha.security.util.EncryptionUtils;
 import io.alpha.service.BaseService;
+import x.y.z.bill.command.LoginForm;
 import x.y.z.bill.command.RealnameForm;
 import x.y.z.bill.command.RegistForm;
 import x.y.z.bill.constant.BizType;
 import x.y.z.bill.constant.IdCardType;
+import x.y.z.bill.model.account.User;
 import x.y.z.bill.service.account.CapitalService;
 import x.y.z.bill.service.account.UserService;
 import x.y.z.bill.util.ExceptionUtil;
@@ -30,6 +33,20 @@ public class AccountService extends BaseService {
             return false;
         }
         return true;
+    }
+
+    public boolean login(final LoginForm loginForm) {
+        String loginId = loginForm.getLoginId();
+        User user;
+        if (loginId.matches("\\d+")) {
+            user = userService.queryByMobile(loginId);
+        } else {
+            user = userService.queryByName(loginId);
+        }
+        if (user != null) {
+            return EncryptionUtils.verifyPassword(loginForm.getPassword(), user.getLoginPwd());
+        }
+        return false;
     }
 
     public boolean realname(final long userId, final RealnameForm realnameForm) {
