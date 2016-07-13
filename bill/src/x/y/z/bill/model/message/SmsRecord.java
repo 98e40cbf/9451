aa66295a0a974d1ab2c55f2b3c55fe494fab25af
fengenius587 <516936742@qq.com>
2016-07-13 14:00:19 +0800

    消息模块

---
+++ bill/src/x/y/z/bill/model/message/SmsRecord.java
@@ -0,0 +1,108 @@
+package x.y.z.bill.model.message;
+
+import java.util.Date;
+import io.alpha.core.model.BaseObject;
+
+public class SmsRecord extends BaseObject {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String txnId;
+
+    private Byte smsType;
+
+    private Byte smsSource;
+
+    private Byte smsStatus;
+
+    private String receiveMobiles;
+
+    private String smsParam;
+
+    private String smsTemplateCode;
+
+    private Byte priority;
+
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTxnId() {
+        return txnId;
+    }
+
+    public void setTxnId(String txnId) {
+        this.txnId = txnId == null ? null : txnId.trim();
+    }
+
+    public Byte getSmsType() {
+        return smsType;
+    }
+
+    public void setSmsType(Byte smsType) {
+        this.smsType = smsType;
+    }
+
+    public Byte getSmsSource() {
+        return smsSource;
+    }
+
+    public void setSmsSource(Byte smsSource) {
+        this.smsSource = smsSource;
+    }
+
+    public Byte getSmsStatus() {
+        return smsStatus;
+    }
+
+    public void setSmsStatus(Byte smsStatus) {
+        this.smsStatus = smsStatus;
+    }
+
+    public String getReceiveMobiles() {
+        return receiveMobiles;
+    }
+
+    public void setReceiveMobiles(String receiveMobiles) {
+        this.receiveMobiles = receiveMobiles == null ? null : receiveMobiles.trim();
+    }
+
+    public String getSmsParam() {
+        return smsParam;
+    }
+
+    public void setSmsParam(String smsParam) {
+        this.smsParam = smsParam == null ? null : smsParam.trim();
+    }
+
+    public String getSmsTemplateCode() {
+        return smsTemplateCode;
+    }
+
+    public void setSmsTemplateCode(String smsTemplateCode) {
+        this.smsTemplateCode = smsTemplateCode == null ? null : smsTemplateCode.trim();
+    }
+
+    public Byte getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Byte priority) {
+        this.priority = priority;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
---
+++ bill/src/x/y/z/bill/model/message/SmsTemplate.java
@@ -0,0 +1,48 @@
+package x.y.z.bill.model.message;
+
+import java.util.Date;
+import io.alpha.core.model.BaseObject;
+
+public class SmsTemplate extends BaseObject {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String templateCode;
+
+    private String templateContent;
+
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTemplateCode() {
+        return templateCode;
+    }
+
+    public void setTemplateCode(String templateCode) {
+        this.templateCode = templateCode == null ? null : templateCode.trim();
+    }
+
+    public String getTemplateContent() {
+        return templateContent;
+    }
+
+    public void setTemplateContent(String templateContent) {
+        this.templateContent = templateContent == null ? null : templateContent.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
