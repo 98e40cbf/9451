a07a4d267f1ca2986a4ec2f391a2f8b91dcc473e
9452 <zcteng@163.com>
2016-07-15 18:26:07 +0800

    #16

--- bill/src/x/y/z/bill/util/IDGenerator.java
+++ bill/src/x/y/z/bill/util/IDGenerator.java
@@ -4,53 +4,11 @@ import io.alpha.util.SequenceHelper;
 
 /**
  * ID生成器
- *
  */
 public class IDGenerator {
-    /**
-     * 产生投资订单ID 规则: 当前时间(14位) + 用户ID(10位，不够前面补0) + 5位定长随机码
-     *
-     * @param userId 用户ID
-     * @return
-     */
+
     public static String generalInvestOrderId(final Long userId) {
         return SequenceHelper.get();
     }
-
-    // /**
-    // * 获取固定长度用户ID(10位,不够前面补0)
-    // *
-    // * @param userId
-    // * @return
-    // *
-    // * @author qiujianfeng 2015年1月12日 下午2:51:07
-    // */
-    // private static String getFixedTenLenthUserId(final String userId) {
-    // StringBuffer buffer = new StringBuffer();
-    // for (int i = 0; i < 10 - userId.length(); i++) {
-    // // 不足5位前面补零
-    // buffer.append("0");
-    // }
-    // buffer.append(userId);
-    // return buffer.toString();
-    // }
-    //
-    // /**
-    // * 获取5位定长随机码
-    // *
-    // * @return
-    // */
-    // private static String getFixedFiveLenthNumber() {
-    // StringBuffer buffer = new StringBuffer("");
-    // // 1-99999随机数
-    // String radomNumber = String.valueOf(new Random().nextInt(99998) + 1);
-    // for (int i = 0; i < 5 - radomNumber.length(); i++) {
-    // // 不足5位前面补零
-    // buffer.append("0");
-    // }
-    // buffer.append(radomNumber);
-    // return buffer.toString();
-    //
-    // }
 
 }
