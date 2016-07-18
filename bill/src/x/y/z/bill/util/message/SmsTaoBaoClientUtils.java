de03557cf0e50b3c9ab4adda22cae41948300810
fengenius587 <516936742@qq.com>
2016-07-18 14:12:12 +0800

    message module

--- bill/src/x/y/z/bill/util/message/SmsTaoBaoClientUtils.java
+++ bill/src/x/y/z/bill/util/message/SmsTaoBaoClientUtils.java
@@ -7,8 +7,9 @@ import com.taobao.api.response.AlibabaAliqinFcSmsNumSendResponse;
 import io.alpha.core.util.PropertiesLoader;
 import io.alpha.logging.Logger;
 import io.alpha.logging.LoggerFactory;
+import io.alpha.util.JsonUtils;
 import x.y.z.bill.constant.message.SmsConstants;
-
+import x.y.z.bill.dto.message.SmsParam;
 
 /**
  * 阿里大鱼
@@ -24,22 +25,25 @@ public final class SmsTaoBaoClientUtils {
     private static String SMS_TAOBAO_APPKEY = PropertiesLoader.getProperty(SmsConstants.PROPS_SMS_TAOBAO_APPKEY);
     private static String SMS_TAOBAO_SECRET = PropertiesLoader.getProperty(SmsConstants.PROSP_SMS_TAOBAO_SECRET);
     private static String SMS_DEFAULT_TYPE = "normal";
-    private static String SMS_TEMPLATE_CODE = "SMS_MESSAGE_001";
-    private static String SMS_FREE_SIGN = "阿里大鱼";
+    private static String SMS_TEMPLATE_CODE = "SMS_12465713";
+    private static String SMS_FREE_SIGN = "刘锋";
 
     public static void sendSms(final String mobile, final String param) {
         TaobaoClient client = new DefaultTaobaoClient(SMS_TAOBAO_URL, SMS_TAOBAO_APPKEY, SMS_TAOBAO_SECRET);
         AlibabaAliqinFcSmsNumSendRequest req = new AlibabaAliqinFcSmsNumSendRequest();
         req.setSmsType(SMS_DEFAULT_TYPE);
         req.setSmsFreeSignName(SMS_FREE_SIGN);
-        req.setSmsParamString(param);
+        SmsParam smsParam = new SmsParam();
+        smsParam.setName(param);
+        req.setSmsParamString(JsonUtils.toJSON(smsParam));
         req.setRecNum(mobile);
         req.setSmsTemplateCode(SMS_TEMPLATE_CODE);
         try {
+            logger.info("短信发送参数:{}",req.getSmsParam());
             AlibabaAliqinFcSmsNumSendResponse rsp = client.execute(req);
-            logger.info("短信发送结果:{}",rsp.getBody());
+            logger.info("短信发送结果:{}", rsp.getBody());
         } catch (Exception t) {
-            logger.error("短信发送异常:{}",t);
+            logger.error("短信发送异常:{}", t);
         }
     }
 }
