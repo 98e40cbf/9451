38d2687f8daccc15e0813875ddef58fd14d25742
fengenius587 <516936742@qq.com>
2016-07-19 12:26:26 +0800

    message module

--- bill/src/x/y/z/bill/service/message/SmsSendRecordService.java
+++ bill/src/x/y/z/bill/service/message/SmsSendRecordService.java
@@ -1,19 +1,17 @@
 package x.y.z.bill.service.message;
 
-import io.alpha.service.BaseService;
-import io.alpha.tx.annotation.TransMark;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import io.alpha.service.BaseService;
+import io.alpha.tx.annotation.TransMark;
 import x.y.z.bill.builder.message.SmsSendRecordBuilder;
 import x.y.z.bill.constant.message.SmsPartnerEnum;
-import x.y.z.bill.constant.message.SmsStatusEnum;
 import x.y.z.bill.mapper.message.SmsSendRecordDAO;
 import x.y.z.bill.model.message.SmsSendRecord;
 import x.y.z.bill.util.ExceptionUtil;
-
-import java.util.Date;
 
 @Service
 @TransMark
--- bill/src/x/y/z/bill/service/message/handler/BaseSmsHandler.java
+++ bill/src/x/y/z/bill/service/message/handler/BaseSmsHandler.java
@@ -1,20 +1,18 @@
 package x.y.z.bill.service.message.handler;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import io.alpha.core.dto.ResultDTO;
 import io.alpha.logging.Logger;
 import io.alpha.logging.LoggerFactory;
 import io.alpha.util.ObjectId;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import x.y.z.bill.builder.message.ResultBuilder;
-import x.y.z.bill.builder.message.SmsSendRecordBuilder;
 import x.y.z.bill.constant.message.ResultTypeEnum;
 import x.y.z.bill.constant.message.SmsPartnerEnum;
 import x.y.z.bill.model.message.SmsRecord;
 import x.y.z.bill.model.message.SmsSendRecord;
 import x.y.z.bill.service.message.SmsSendRecordService;
-
-import static org.bouncycastle.asn1.x500.style.RFC4519Style.l;
 
 @Service
 public abstract class BaseSmsHandler {
