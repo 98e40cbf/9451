7abbe5bd015536184c72b1a2c62de2df523d872f
9452 <zcteng@163.com>
2016-07-21 21:06:53 +0800

    #21

--- bill/src/x/y/z/bill/service/account/Accounting.java
+++ bill/src/x/y/z/bill/service/account/Accounting.java
@@ -2,17 +2,19 @@ package x.y.z.bill.service.account;
 
 import java.math.BigDecimal;
 
-import x.y.z.bill.constant.BizType;
 import io.alpha.core.model.BaseObject;
 import io.alpha.util.DecimalUtil;
+import x.y.z.bill.constant.BizType;
 
 class Accounting extends BaseObject {
 
     private static final long serialVersionUID = 4638129767959186548L;
+    private final Long userId;
     private final BizType bizType;
     private final BigDecimal amount;
 
-    public Accounting(final BizType bizType, final BigDecimal amount) {
+    public Accounting(final Long userId, final BizType bizType, final BigDecimal amount) {
+        this.userId = userId;
         this.bizType = bizType;
         this.amount = DecimalUtil.format(amount);
     }
@@ -23,6 +25,10 @@ class Accounting extends BaseObject {
 
     public BigDecimal getAmount() {
         return amount;
+    }
+
+    public Long getUserId() {
+        return userId;
     }
 
 }
